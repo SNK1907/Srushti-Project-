a31814e048d87cffe0c1c3fd99354229e55a5f7c
SNK1907 <srushtikamalkar19@gmail.com>
2026-09-18 13:15:55 +0530

    Add Variable_Names.py

---
+++ srushti_2_0/01_Basics/Variable_Names.py
@@ -0,0 +1,12 @@
+# Author: Srushti
+# Project: python-lab
+#Camel Case
+myVariableName = "Srushti"
+
+#Pascal Case
+MyVariableName = "Subhash"
+
+#Snake Case
+my_variable_name = "More"
+
+print(myVariableName , MyVariableName , my_variable_name) 
